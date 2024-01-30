879bed54d1992192595b3e339ae4377de92f0f1c
Nathan Q <natethegreat08q@gmail.com>
2024-01-30 16:45:18 -0500

    updated IntakeSubsystem.java

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -1,36 +1,45 @@
 package frc.robot.subsystems.intake;
 
-import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.SparkPIDController.AccelStrategy;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
-    //public CANSparkMax m_intakeMotor;
+    private final CANSparkMax m_intakeMotor;
+
+    private final RelativeEncoder m_encoder;
+
+    private final SparkPIDController m_pidController;
     
     public IntakeSubsystem(int intakeMotorPort) {
-        //m_intakeMotor = new CANSparkMax(intakeMotorPort, CANSparkLowLevel.MotorType.kBrushless);
+        m_intakeMotor = new CANSparkMax(intakeMotorPort, MotorType.kBrushless);
+
+        m_intakeMotor.setIdleMode(IdleMode.kCoast);
+
+        m_pidController = m_intakeMotor.getPIDController();
+
+        m_pidController.setSmartMotionAccelStrategy(AccelStrategy.kTrapezoidal, 0);
     }
     
     public void startIntake() {
-        //m_intakeMotor.set(0.5);
+        m_pidController.setReference(1, ControlType.kSmartVelocity);
     }
     
     public void stopIntake() {
-        //m_intakeMotor.set(0.0);
+        m_pidController.setReference(0, ControlType.kSmartVelocity);
     }
     
     public Command getStartIntakeCommand() {
-        System.out.println("HIIIII");
-        return this.runOnce(() -> System.out.println("INTAKING!!!!!!!!!!!"));
+        return this.runOnce(() -> startIntake()).withName("startIntakeCommand");
     }
     
     public Command getStopIntakeCommand() {
         return this.runOnce(() -> stopIntake()).withName("stopIntakeCommand");
-    }
-
-    @Override
-    public void periodic() {
-        
     }
 }
