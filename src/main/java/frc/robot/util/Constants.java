cb2bc57c9da22e7829edfb15b408308586fc2be1
Gladiator Robotics <jchsrobotics@gmail.com>
2024-03-02 00:58:47 -0500

    it works!

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -112,7 +112,7 @@ public final class Constants {
       public static final double kDriveI = 0;
       public static final double kDriveD = 0.05;
   
-      public static final double kTurnP = 0.1;
+      public static final double kTurnP = 1.5;
       public static final double kTurnI = 0;
       public static final double kTurnD = 0;
     
