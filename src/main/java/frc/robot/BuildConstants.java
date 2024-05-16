d28aab95681d09c7de728770547e7ef527330228
Nathan Q <Natethegreat08q@gmail.com>
2024-05-16 13:27:56 -0400

    install advantage kit

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "SwerveBase";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 62;
-	public static final String GIT_SHA = "8deed902f303bd830190d72b1bf3a7fa58ab8157";
-	public static final String GIT_DATE = "2024-05-16 13:16:16 EDT";
+	public static final int GIT_REVISION = 63;
+	public static final String GIT_SHA = "1d6c995b3540b8e5d2fd9197803a5ef8ac549f8c";
+	public static final String GIT_DATE = "2024-05-16 13:21:25 EDT";
 	public static final String GIT_BRANCH = "AdvantageKit";
-	public static final String BUILD_DATE = "2024-05-16 13:21:09 EDT";
-	public static final long BUILD_UNIX_TIME = 1715880069999L;
+	public static final String BUILD_DATE = "2024-05-16 13:27:22 EDT";
+	public static final long BUILD_UNIX_TIME = 1715880442022L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {
---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Time;
+import edu.wpi.first.units.Units;
+
+public final class Constants {
+	public static final Measure<Time> kRobotLoopPeriod = Units.Seconds.of(Robot.defaultPeriodSecs);
+}
