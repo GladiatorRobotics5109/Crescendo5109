1d6c995b3540b8e5d2fd9197803a5ef8ac549f8c
Nathan Q <Natethegreat08q@gmail.com>
2024-05-16 13:21:25 -0400

    setup gradle proj

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+	public static final String MAVEN_GROUP = "";
+	public static final String MAVEN_NAME = "SwerveBase";
+	public static final String VERSION = "unspecified";
+	public static final int GIT_REVISION = 62;
+	public static final String GIT_SHA = "8deed902f303bd830190d72b1bf3a7fa58ab8157";
+	public static final String GIT_DATE = "2024-05-16 13:16:16 EDT";
+	public static final String GIT_BRANCH = "AdvantageKit";
+	public static final String BUILD_DATE = "2024-05-16 13:21:09 EDT";
+	public static final long BUILD_UNIX_TIME = 1715880069999L;
+	public static final int DIRTY = 1;
+
+	private BuildConstants() {
+	}
+}
