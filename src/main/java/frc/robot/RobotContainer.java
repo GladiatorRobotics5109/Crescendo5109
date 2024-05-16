8deed902f303bd830190d72b1bf3a7fa58ab8157
Nathan Q <Natethegreat08q@gmail.com>
2024-05-16 13:16:16 -0400

    create new proj

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,74 +4,17 @@
 
 package frc.robot;
 
-import frc.robot.Constants.DriveTeamConstants;
-import frc.robot.auton.AutonFactory;
-import frc.robot.subsystems.swerve.SwerveSubsystem;
-
-import java.nio.file.FileSystem;
-
-import com.pathplanner.lib.path.PathPlannerPath;
-
-import edu.wpi.first.math.filter.SlewRateLimiter;
-import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(DriveTeamConstants.kDriverControllerPort);
-
-  private final SlewRateLimiter m_driverXLimiter = new SlewRateLimiter(20);
-  private final SlewRateLimiter m_driverYLimiter = new SlewRateLimiter(20);
-  private final SlewRateLimiter m_driverRotLimiter = new SlewRateLimiter(10);
-
-  private final SwerveSubsystem m_swerve;
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // instantiate swerve
-    m_swerve = new SwerveSubsystem();
-
-    m_swerve.setDefaultCommand(
-      m_swerve.getDriveWithJoystickCommand(
-        () -> m_driverXLimiter.calculate(m_driverController.getLeftX()), // l/r
-        () -> m_driverYLimiter.calculate(-m_driverController.getLeftY()), // f/b
-        () -> m_driverRotLimiter.calculate(-m_driverController.getRightX()), // rot
-        () -> m_driverController.getLeftTriggerAxis(), // super speed
-        () -> m_driverController.getRightTriggerAxis(), // super slow
-        () -> true) // field relative
-    );
-
-    
-    // Configure the controller bindings
-    configureButtonBindings();
+    configureBindings();
   }
 
-  /** 
-   * Configure button bindings for controllers (axis bindings may not be handled by this method)
-  */
-  private void configureButtonBindings() {
-    m_driverController.x().onTrue(m_swerve.getBrakeAndXCommand());
-    m_driverController.a().whileTrue(m_swerve.getAlignWheelCommand());
-  }
+  private void configureBindings() {}
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
-    return AutonFactory.getTestAuton(m_swerve);
-  }
-
-  public void resetEncoders() {
-    m_swerve.resetEncoders();
+    return Commands.print("No autonomous command configured");
   }
 }
